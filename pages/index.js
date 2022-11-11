273aeb7e1e7a1717caa73ffca9e41e05e2e2fb2e
kayacancode <62274079+kayacancode@users.noreply.github.com>
2022-11-11 17:18:05 -0600

    nextlink

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import {useState, react} from 'react'
-import Link from 'next/Link'
+import Link from 'next/link'
 export default function Home() {
   const [state, setState] = useState(false)
 
